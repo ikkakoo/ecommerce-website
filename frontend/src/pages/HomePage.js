6422720143df17d4f8974f467f208137dde7e4b7
ikkakoo <dzvelaia.irakli1@gmail.com>
2022-09-13 17:03:59 +0400

    made first api call to drf api

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Flex, Box, Heading, Text, Container } from '@chakra-ui/react';
 import Product from '../components/Product';
-
-import products from '../products';
+import axios from 'axios';
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const { data } = await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+    }
+
+    getProducts();
+  }, [])
+
   return (
     <>
       <Heading mb={5}>Latest Products</Heading>
