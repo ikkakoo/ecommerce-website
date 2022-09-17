b35742dfc09b721d5a733af2f6bd24621bc0606b
ikkakoo <dzvelaia.irakli1@gmail.com>
2022-09-17 23:32:09 +0400

    added cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<HomePage />} exact />
-            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="product/:id" element={<ProductPage />} />
+            <Route path="cart/:id?" element={<CartPage />} />
           </Routes>
         </main>
         <Footer />
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import {
   Box,
   Button,
@@ -10,82 +10,110 @@ import {
   Text,
   SimpleGrid,
   Container,
-  Tooltip,
+  Select,
 } from '@chakra-ui/react';
 import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { FaAngleLeft, FaCartPlus } from 'react-icons/fa';
-import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux';
+import { listProductDetails } from '../actions/productActions';
 
 const ProductPage = () => {
-  const [product, setProduct] = useState([])
+  const [qty, setQty] = useState(1);
 
   const { id } = useParams();
 
-  console.log(id);
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector(state => state.productDetails);
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    const getProduct = async () => {
-      const { data } = await axios.get(`/api/products/${id}`)
-      setProduct(data);
-    }
+    dispatch(listProductDetails(id));
+  }, []);
 
-    getProduct();
-  }, [])
-  
+  const navigate = useNavigate()
+  const addToCartHandler = () => {
+    navigate(`/cart/${id}?qty=${qty}`)
+  };
 
   return (
     <Container maxW={'container.xl'} p={10}>
-      <SimpleGrid columns={3} spacing={'2em'}>
-        <Box>
-          <Link to="/">
-            <Button colorScheme="teal" variant="outline" mb={6}>
-              <Icon as={FaAngleLeft}></Icon>
-              Go Back
+      <Link to="/">
+        <Button colorScheme="teal" variant="outline" mb={6}>
+          <Icon as={FaAngleLeft}></Icon>
+          Go Back
+        </Button>
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message status={'error'} error={error} title="Error" />
+      ) : (
+        <SimpleGrid columns={3} spacing={'2em'}>
+          <Box>
+            <Link to={`/product/${product.id}`}>
+              <Image
+                src={product.image}
+                rounded="md"
+                fit="cover"
+                cursor={'pointer'}
+              ></Image>
+            </Link>
+          </Box>
+
+          <Flex gap={10} direction="column">
+            <Heading>{product.name}</Heading>
+            <Rating
+              rating={product.rating}
+              text={`Total of ${product.numReviews} reviews`}
+              color="#f7d600"
+            />
+
+            <Heading as="h2" size="lg">
+              $ {product.price}
+            </Heading>
+
+            <Text textAlign="center" minHeight="50px" cursor={'pointer'}>
+              Description: {product.description}
+            </Text>
+          </Flex>
+
+          <Box>
+            <Text>Price: ${product.price}</Text>
+            <Text>
+              Stock:{' '}
+              {product.count_in_stock === 0
+                ? 'Out of Stock'
+                : product.count_in_stock}
+            </Text>
+
+            {product.count_in_stock > 0 && (
+              <Select
+                placeholder="Select Quantity"
+                value={qty}
+                onChange={e => setQty(e.target.value)}
+              >
+                {[...Array(product.count_in_stock).keys()].map(x => (
+                  <option key={x + 1} value={x + 1}>
+                    {x + 1}
+                  </option>
+                ))}
+              </Select>
+            )}
+
+            <Button
+              onClick={addToCartHandler}
+              disabled={product.count_in_stock == 0}
+              leftIcon={<FaCartPlus />}
+              variant="outline"
+            >
+              Add to Cart
             </Button>
-          </Link>
-
-          <Link to={`/product/${product.id}`}>
-            <Image
-              src={product.image}
-              rounded="md"
-              fit="cover"
-              cursor={'pointer'}
-            ></Image>
-          </Link>
-        </Box>
-
-        <Flex gap={10} direction="column">
-          <Heading>{product.name}</Heading>
-          <Rating
-            rating={product.rating}
-            text={`Total of ${product.numReviews} reviews`}
-            color="#f7d600"
-          />
-
-          <Heading as="h2" size="lg">
-            $ {product.price}
-          </Heading>
-
-          <Text textAlign="center" minHeight="50px" cursor={'pointer'}>
-            Description: {product.description}
-          </Text>
-        </Flex>
-
-        <Box>
-          <Text>Price: ${product.price}</Text>
-          <Text>
-            Stock:{' '}
-            {product.countInStock === 0 ? 'Out of Stock' : product.countInStock}
-          </Text>
-          <Button
-            disabled={product.countInStock == 0}
-            leftIcon={<FaCartPlus />}
-            variant="outline"
-          >
-            Add to Cart
-          </Button>
-        </Box>
-      </SimpleGrid>
+          </Box>
+        </SimpleGrid>
+      )}
     </Container>
   );
 };
