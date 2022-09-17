6564f68af9677a58e309ef2465fdc5f9a0701159
ikkakoo <dzvelaia.irakli1@gmail.com>
2022-09-17 18:21:56 +0400

    implemented redux, created redux store, product reducers and product actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,17 @@ import { ColorModeScript, ChakraProvider, theme } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  // <StrictMode>
+  <Provider store={store}>
     <ChakraProvider theme={theme}>
       <ColorModeScript />
       <App />
     </ChakraProvider>
-  // </StrictMode>
+  </Provider>
 );
