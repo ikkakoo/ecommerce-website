990b4e89560a101a9d9c1cb872a5385d339e2c6e
ikkakoo <dzvelaia.irakli1@gmail.com>
2022-09-18 18:24:23 +0400

    created serializer for user model

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -1,8 +1,39 @@
-from dataclasses import fields
-from pyexpat import model
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from main.models import Product
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    is_admin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'is_admin']
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+
+        return name
+
+    def get_is_admin(self, obj):
+        return obj.is_staff
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'is_admin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from main.views import get_product, get_products, get_routes, MyTokenObtainPairView
+from main.views import get_product, get_products, get_routes, MyTokenObtainPairView, get_user_profile
 
 urlpatterns = [
     path('users/login/', MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
+         
 
     path('', get_routes, name='routes'),
+
+    path('users/profile/', get_user_profile, name='user-profile'),
+
     path('products/', get_products, name='products'),
     path('products/<str:pk>', get_product, name='product'),
 ]
