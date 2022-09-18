2d0d2c88942789c0c81afd81d14db0dc6113e685
ikkakoo <dzvelaia.irakli1@gmail.com>
2022-09-18 17:57:07 +0400

    implemented jwt tokens for backend user authentication

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from main.views import get_product, get_products, get_routes
+from main.views import get_product, get_products, get_routes, MyTokenObtainPairView
 
 urlpatterns = [
+    path('users/login/', MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+
     path('', get_routes, name='routes'),
     path('products/', get_products, name='products'),
     path('products/<str:pk>', get_product, name='product'),
